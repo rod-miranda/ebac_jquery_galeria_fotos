e3ade907715c9bd3ac58bb1ab555941de604124d
Rodrigo Miranda <rodmiranda_@outlook.com>
2024-04-08 15:39:34 -0300

    Adição de arquivos do exercício proposto
    Para tirar dúvidas com tutor

--- main.js
+++ main.js
@@ -9,18 +9,9 @@ $(document).ready(function(){
 
     $('form').on('submit', function(e) {
         e.preventDefault();
-        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
-        const novoItem = $('<li style="display: none"></li>');
-        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
-        $(`
-            <div class="overlay-imagem-link">
-                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
-                    Ver imagem em tamanho real
-                </a>
-            </div>
-        `).appendTo(novoItem);
-        $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn(1000);
-        $('#endereco-imagem-nova').val('');
+
+        const tarefaAdicionada = $('#nova-tarefa').val();
+        $(tarefaAdicionada).appendTo('ul');
+        $('#nova-tarefa').val('');
     })
 })
